98cb75fee8a49e4d30e6671a20d7fd01bb45378a
sinthyasithy <145050483+sinthyasithy@users.noreply.github.com>
2024-04-29 21:27:10 +0600

    initial commit

---
+++ color_json/index.js
@@ -0,0 +1,43 @@
+let colorList = {
+  "black": {
+    "category": "hue",
+    "type": "primary",
+    "code": {
+      "rgba": [255, 255, 255, 1],
+      "hex": "#000"
+    }
+  },
+  "white": {
+    "category": "value",
+    "type": "primary",
+    "code": {
+      "rgba": [0, 0, 0, 1],
+      "hex": "#FFF"
+    }
+  },
+  "red": {
+    "category": "hue",
+    "type": "primary",
+    "code": {
+      "rgba": [255, 0, 0, 1],
+      "hex": "#FF0"
+    }
+  },
+  
+  "green": {
+    "category": "hue",
+    "type": "secondary",
+    "code": {
+      "rgba": [0, 255, 0, 1],
+      "hex": "#0F0"
+    }
+  }
+};
+// console.log(colorList.colors[1]);
+// console.log(colorList.colors[2].type);
+// console.log(colorList.colors[3].code);
+// console.log(colorList.colors[4].code.rgba);
+// console.log(colorList.colors[5].code.hex);
+for(propty in colorList){
+  console.log(propty + ': '+ colorList[propty]);
+}
